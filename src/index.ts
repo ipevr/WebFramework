01fbceedb3cc7ae7ce6f71cec74566fa9a0c369e
Ingo Peters <ingo.peters@iav.de>
2022-02-17 18:20:29 +0100

    Testing of correct event registering

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,14 @@ const user = new User({});
 
 user.set({name: "Jim", age: 35});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
-
-user.set({age: 38});
-
-console.log(user.get("name"));
-console.log(user.get("age"));
-
+user.on("change", () => {
+    console.log("Changed!");
+});
+user.on("change", () => {
+    console.log("Do something when changed!");
+});
 user.on("click", () => {
     console.log("Clicked!");
-})
+});
 
-console.log(user.events);
+console.log(user);
