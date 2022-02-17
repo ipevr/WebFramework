0f6c6791492500b3ef8a4e475ea78faba9d7c741
Ingo Peters <ingo.peters@iav.de>
2022-02-17 17:37:03 +0100

    set method implemented
    - Implemented a set method to set the user's new properties
    - Made user properties optional, so that don't have to set all user properties, but only those which have changed. Or even no properties, when you initialize the class.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import {User} from "./models/user";
 
-const user = new User({name: "Ingo", age: 53});
+const user = new User({});
+
+user.set({name: "Jim", age: 35});
 
 console.log(user.get("name"));
+console.log(user.get("age"));
+
+user.set({age: 38});
+
+console.log(user.get("name"));
+console.log(user.get("age"));
